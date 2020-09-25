cff0611dedb112df33810cf390f96b32a9a1fa45
shannonphu <shannonphu@gmail.com>
2020-09-24 23:46:23 -0700

    fix warnings

--- app/src/components/dashboard/dashboard.tsx
+++ app/src/components/dashboard/dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { TweetListItem } from '../tweet-list-item/tweet-list-item';
 import Container from '@material-ui/core/Container';
 import List from '@material-ui/core/List';
